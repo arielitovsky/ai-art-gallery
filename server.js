b6f534aee79b35e82f1ca6f927256c0d56c72db9
Arie Litovsky <arie.litovsky@gmail.com>
2023-01-04 14:19:05 -0600

    Initial early idea (just generated a purple dog with openAI)

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const getters = require('./src/getters.js')
+
+var express = require('express');
+var app = express();
+
+
+
+app.get('/', function (req, res) {
+   res.send('Welcome to the AI art gallery');
+})
+
+
+app.get('/generate', getters.generateImage)
+
+
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Server is running on port http://%s:%s", host, port)
+})
+
---
+++ src/getters.js
@@ -0,0 +1,21 @@
+const openai = require('openai')
+
+const configuration = new openai.Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openaiApi = new openai.OpenAIApi(configuration);
+
+async function generateImage(req, res) {
+
+  const response = await openaiApi.createImage({
+    prompt: "a cute purple dog doing laps",
+    n: 1,
+    size: "512x512",
+  })
+
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ urrl: response.data.data[0].url }));
+}
+
+module.exports = {generateImage}
